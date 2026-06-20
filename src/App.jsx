7e537644f4ee1fda7bbc664663dc91c767f4978b
Keerthana <vallurupallikeerthana46@gmail.com>
2026-06-20 11:20:19 +0530

    modified

--- src/App.jsx
+++ src/App.jsx
@@ -2,18 +2,19 @@ import Home from './components/Home'
 import About from './components/About'
 import './App.css'
 
-function App() {
+export default function App() {
   // const [count, setCount] = useState(0)
+  let isLogin = true 
 
   return (
     <>
     <h1>This is the App component</h1>
-      <Home />
+      {/* <Home />
       <p>hii</p>
-      <About />
+      <About /> */}
+      { isLogin ? <Home /> : <About /> }
     </>
   
   )
 }
 
-export default App
