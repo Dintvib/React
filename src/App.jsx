7e95f6043a22e9e088d53f969765454db4cbe9c6
Keerthana <vallurupallikeerthana46@gmail.com>
2026-06-20 11:05:28 +0530

    added

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,18 @@
-// import { useState } from 'react'
-// import reactLogo from './assets/react.svg'
-// import viteLogo from './assets/vite.svg'
-// import heroImg from './assets/hero.png'
+import Home from './components/Home'
+import About from './components/About'
 import './App.css'
 
 function App() {
   // const [count, setCount] = useState(0)
 
   return (
-    <><h1>hello keerthi</h1><h2>keerthi</h2></>
+    <>
+    <h1>This is the App component</h1>
+      <Home />
+      <p>hii</p>
+      <About />
+    </>
+  
   )
 }
 
---
+++ src/components/About.jsx
@@ -0,0 +1,8 @@
+export default function About() {
+    let name = "About"
+    return (
+        <>
+            <h1>This is the {name} component</h1>
+        </>
+    )
+}
